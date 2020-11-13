a1e848f84559b8df243498fed4059780e4b9656e
stefaleon <stefaleon@gmail.com>
2020-11-13 14:58:06 +0200

    Export connect() to a module

---
+++ connect.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connect = async (dbConnectionString) => {
+  try {
+    await mongoose.connect(dbConnectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    const conn = mongoose.connection;
+    console.log(`Connected to the "${conn.name}"  database`);
+    return conn;
+  } catch (error) {
+    console.log('Error on db connection - Code:', error.code);
+  }
+};
+
+module.exports = connect;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const mongoose = require('mongoose');
+
+const connect = require('./connect');
+
 require('dotenv').config();
 
 const app = express();
@@ -8,22 +10,8 @@ app.get('/', (req, res) => {
   res.send('This is the Asset Manager API');
 });
 
-const connect = async () => {
-  try {
-    await mongoose.connect(process.env.DB, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    const conn = mongoose.connection;
-    console.log(`Connected to the "${conn.name}"  database`);
-    return conn;
-  } catch (error) {
-    console.log('Error on db connection - Code:', error.code);
-  }
-};
-
 const listen = async () => {
-  const conn = await connect();
+  const conn = await connect(process.env.DB);
   if (conn) {
     app.listen(process.env.PORT, () => {
       console.log(`Server running on port ${process.env.PORT}`);
