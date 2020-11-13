0b1c7fa2a0f2d85049f2afdaa8cdde2c6ce8671d
stefaleon <stefaleon@gmail.com>
2020-11-13 14:52:59 +0200

    Connect to the Database

---
+++ README.md
@@ -0,0 +1,8 @@
+## Environment variables
+
+- Environment variables are read from a .env file in the root folder with the following content:
+
+```
+PORT=5000
+DB=mongodb+srv://USERNAME:PASSWORD@cluster0.pphyl.mongodb.net/DB_NAME?retryWrites=true&w=majority
+```
--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Express!');
+  res.send('This is the Asset Manager API');
 });
+
+try {
+  mongoose.connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+} catch (error) {
+  console.log(error);
+}
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
