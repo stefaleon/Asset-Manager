2b9e1888756e97cfb578247c95aacc2f6f8d5c7b
unknown <stefaleon@gmail.com>
2020-11-15 02:18:33 +0200

    Cannot set headers after they are sent to the client

--- controllers/categories.js
+++ controllers/categories.js
@@ -22,7 +22,7 @@ exports.readCategory = async (req, res, next) => {
   try {
     const category = await Category.findById(req.params.id);
     if (!category) {
-      res.status(404).json({ error: 'Category not found' });
+      return res.status(404).json({ error: 'Category not found' });
     }
     res.status(200).json({ data: category });
   } catch (err) {
