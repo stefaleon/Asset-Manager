6e182eaddd5a6697e633155d969c55bac7fb2f5f
unknown <stefaleon@gmail.com>
2020-11-18 20:09:39 +0200

    React Router

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import { Container } from 'react-bootstrap';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import NavigationBar from './components/NavigationBar';
 
 function App() {
   return (
-    <Container>
-      <NavigationBar />
-    </Container>
+    <BrowserRouter>
+      <Container>
+        <NavigationBar />
+        <Switch>
+          <Route exact path='/'>
+            <div>Root</div>
+          </Route>
+        </Switch>
+      </Container>
+    </BrowserRouter>
   );
 }
 
