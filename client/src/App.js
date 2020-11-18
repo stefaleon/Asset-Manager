0c36cdbc928941a4a875444da2d6f26de9192e9b
unknown <stefaleon@gmail.com>
2020-11-18 19:10:30 +0200

    Navigation Bar Component with React Bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+import { Container } from 'react-bootstrap';
+import NavigationBar from './components/NavigationBar';
+
 function App() {
-  return <div className='App'>Asset manager</div>;
+  return (
+    <Container>
+      <NavigationBar />
+    </Container>
+  );
 }
 
 export default App;
