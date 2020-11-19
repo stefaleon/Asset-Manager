31f2f8f127eca55f9767d7ad9a252d8b6bba785e
unknown <stefaleon@gmail.com>
2020-11-19 18:19:59 +0200

    LinkContainers in the NavigationBar

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -1,27 +1,48 @@
 import React from 'react';
 
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const NavigationBar = () => {
   return (
-    <Navbar bg='light' expand='lg'>
-      <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
+    <Navbar expand='lg' bg='dark' variant='dark'>
+      <LinkContainer to='/'>
+        <Navbar.Brand>Asset Manager</Navbar.Brand>
+      </LinkContainer>
       <Navbar.Toggle aria-controls='basic-navbar-nav' />
-      <Navbar.Collapse id='basic-navbar-nav'>
+      <Navbar.Collapse>
         <Nav className='mr-auto'>
-          <Nav.Link href='#home'>Home</Nav.Link>
-          <Nav.Link href='#link'>Link</Nav.Link>
-          <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
-            <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
-            <NavDropdown.Item href='#action/3.2'>
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href='#action/3.3'>Something</NavDropdown.Item>
+          <LinkContainer to='/about'>
+            <Nav.Link>
+              <i className='fas fa-info-circle'></i> About
+            </Nav.Link>
+          </LinkContainer>
+          <NavDropdown title='Manage'>
+            <LinkContainer to='/assets'>
+              <NavDropdown.Item>
+                <i className='fas fa-circle mr-2'></i> Assets
+              </NavDropdown.Item>
+            </LinkContainer>
             <NavDropdown.Divider />
-            <NavDropdown.Item href='#action/3.4'>
-              Separated link
-            </NavDropdown.Item>
+            <LinkContainer to='/categories'>
+              <NavDropdown.Item>
+                <i className='fas fa-globe mr-2'></i> Categories
+              </NavDropdown.Item>
+            </LinkContainer>
+            <NavDropdown.Divider />
+            <LinkContainer to='/locations'>
+              <NavDropdown.Item>
+                <i className='fas fa-compass mr-2'></i> Locations
+              </NavDropdown.Item>
+            </LinkContainer>
           </NavDropdown>
+        </Nav>
+        <Nav>
+          <LinkContainer to='/login'>
+            <Nav.Link>
+              Sign In <i className='fas fa-sign-in-alt'></i>
+            </Nav.Link>
+          </LinkContainer>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
