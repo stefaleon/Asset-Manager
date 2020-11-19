f6d37a60a361d996ec1158a7bc61958d52a4a75d
unknown <stefaleon@gmail.com>
2020-11-19 22:40:01 +0200

    Tabs and Components for Tabs

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-import { Container } from 'react-bootstrap';
+import { Container, Tabs, Tab } from 'react-bootstrap';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import NavigationBar from './components/NavigationBar';
 import NotFound from './components/NotFound';
 import About from './components/About';
+import Assets from './components/Assets';
+import ByCategory from './components/ByCategory';
+import ByLocation from './components/ByLocation';
 
 function App() {
   return (
@@ -15,7 +18,17 @@ function App() {
         <Switch>
           <Route path='/about' component={About} />
           <Route exact path='/'>
-            <div>Root</div>
+            <Tabs defaultActiveKey='assets' id='tabs'>
+              <Tab eventKey='assets' title='Assets'>
+                <Assets />
+              </Tab>
+              <Tab eventKey='by-category' title='ByCategory'>
+                <ByCategory />
+              </Tab>
+              <Tab eventKey='by-location' title='ByLocation'>
+                <ByLocation />
+              </Tab>
+            </Tabs>
           </Route>
           <Route path='/*' component={NotFound} />
         </Switch>
