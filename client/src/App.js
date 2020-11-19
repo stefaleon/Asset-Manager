00beb7cbf44a82fdfe048492a41fdf776f32c515
unknown <stefaleon@gmail.com>
2020-11-19 22:08:51 +0200

    About Component and CSS overrides on Bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { Container } from 'react-bootstrap';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import NavigationBar from './components/NavigationBar';
 import NotFound from './components/NotFound';
+import About from './components/About';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Container>
         <NavigationBar />
         <Switch>
+          <Route path='/about' component={About} />
           <Route exact path='/'>
             <div>Root</div>
           </Route>
---
+++ client/src/components/About.js
@@ -0,0 +1,11 @@
+import { Jumbotron } from 'react-bootstrap';
+
+const About = () => {
+  return (
+    <Jumbotron>
+      <div>Asset Manager v1.0.1</div>
+    </Jumbotron>
+  );
+};
+
+export default About;
