2f6225c6d47b81cd2e99804029671ff114469f41
unknown <stefaleon@gmail.com>
2020-12-01 19:56:05 +0200

    Password Hashing with BcryptJs

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,14 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
 
 exports.createUser = async (req, res, next) => {
   try {
-    const user = await new User(req.body).save();
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = await new User({
+      ...req.body,
+      password: hashedPassword,
+    }).save();
     res.status(200).json({ data: user });
   } catch (err) {
     next(err);
