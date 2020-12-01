0e673bb1b3215be2c4aadd21dce066c617df6e9a
unknown <stefaleon@gmail.com>
2020-12-01 16:48:10 +0200

    Validation in the Database

--- models/user.js
+++ models/user.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
 
+const validateEmail = require('../utils/validate-email');
+
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      required: 'Email address is required',
+      validate: [validateEmail, 'Please use a valid email address'],
+    },
+    password: {
+      type: String,
+      required: 'Password is required',
+      minlength: [6, 'Minimum password length is six characters'],
+    },
     admin: { type: Boolean, default: false },
   },
   {
