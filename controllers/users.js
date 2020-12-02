4e4e6ffe0607cf25cbd352a4de9f0db9064ddb1a
unknown <stefaleon@gmail.com>
2020-12-02 18:06:07 +0200

    Login User

--- controllers/users.js
+++ controllers/users.js
@@ -39,3 +39,30 @@ exports.createUser = async (req, res, next) => {
     next(err);
   }
 };
+
+exports.loginUser = async (req, res, next) => {
+  try {
+    if (!req.body.email || !validateEmail(req.body.email.trim())) {
+      return res.status(400).json({ error: 'Please enter the user email' });
+    }
+
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(400).json({ error: 'Wrong Credentials' });
+    }
+
+    const compare = await bcrypt.compare(
+      req.body.password || '',
+      user.password
+    );
+
+    if (!compare) {
+      return res.status(400).json({ error: 'Wrong Credentials' });
+    }
+
+    res.status(200).json({ token: user.generateToken() });
+  } catch (err) {
+    next(err);
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const usersController = require('../controllers/users');
 
 router.post('/', usersController.createUser);
+router.post('/login', usersController.loginUser);
 
 module.exports = router;
