d17c04e39a84a9f1a1bdfd8d0746a60b26b4aa3b
unknown <stefaleon@gmail.com>
2020-12-06 12:31:55 +0200

    Reconfigure the NavigationBar

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -1,9 +1,13 @@
-import React from 'react';
-
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
-const NavigationBar = () => {
+const NavigationBar = ({ token, username, admin, dispatch, logoutUser }) => {
+  const onLogoutHandler = () => {
+    if (window.confirm('Confirm User Sign Out')) {
+      logoutUser(dispatch);
+    }
+  };
+
   return (
     <Navbar expand='lg' bg='dark' variant='dark'>
       <LinkContainer to='/'>
@@ -17,32 +21,66 @@ const NavigationBar = () => {
               <i className='fas fa-info-circle'></i> About
             </Nav.Link>
           </LinkContainer>
-          <NavDropdown title='Manage'>
-            <LinkContainer to='/assets'>
-              <NavDropdown.Item>
-                <i className='fas fa-circle mr-2'></i> Assets
-              </NavDropdown.Item>
-            </LinkContainer>
-            <NavDropdown.Divider />
-            <LinkContainer to='/categories'>
-              <NavDropdown.Item>
-                <i className='fas fa-globe mr-2'></i> Categories
-              </NavDropdown.Item>
-            </LinkContainer>
-            <NavDropdown.Divider />
-            <LinkContainer to='/locations'>
-              <NavDropdown.Item>
-                <i className='fas fa-compass mr-2'></i> Locations
-              </NavDropdown.Item>
-            </LinkContainer>
-          </NavDropdown>
+          {token && (
+            <NavDropdown title='Manage'>
+              <LinkContainer to='/assets'>
+                <NavDropdown.Item>
+                  <i className='fas fa-circle mr-2'></i> Assets
+                </NavDropdown.Item>
+              </LinkContainer>
+              <NavDropdown.Divider />
+              <LinkContainer to='/categories'>
+                <NavDropdown.Item>
+                  <i className='fas fa-globe mr-2'></i> Categories
+                </NavDropdown.Item>
+              </LinkContainer>
+              <NavDropdown.Divider />
+              <LinkContainer to='/locations'>
+                <NavDropdown.Item>
+                  <i className='fas fa-compass mr-2'></i> Locations
+                </NavDropdown.Item>
+              </LinkContainer>
+              {admin && (
+                <>
+                  <NavDropdown.Divider />
+                  <LinkContainer to='/users'>
+                    <NavDropdown.Item>
+                      <i className='fas fa-user-circle mr-2'></i> Users
+                    </NavDropdown.Item>
+                  </LinkContainer>
+                </>
+              )}
+            </NavDropdown>
+          )}
         </Nav>
         <Nav>
-          <LinkContainer to='/login'>
+          <LinkContainer to='/userdata'>
             <Nav.Link>
-              Sign In <i className='fas fa-sign-in-alt'></i>
+              <div
+                style={{
+                  color: 'white',
+                  display: token ? 'inline-block' : 'none',
+                  background: admin ? 'red' : 'blue',
+                  borderRadius: '5%',
+                  padding: 2,
+                  fontWeight: 'bold',
+                }}
+              >
+                {username?.split(' ').map((x) => x.slice(0, 1).toUpperCase())}
+              </div>
             </Nav.Link>
           </LinkContainer>
+          {token ? (
+            <Nav.Link onClick={onLogoutHandler}>
+              <i className='fas fa-sign-out-alt'></i> Sign Out
+            </Nav.Link>
+          ) : (
+            <LinkContainer to='/login'>
+              <Nav.Link>
+                Sign In <i className='fas fa-sign-in-alt'></i>
+              </Nav.Link>
+            </LinkContainer>
+          )}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
